1c50942fa916b01f8660e4c688ad04d556058765
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-04-15 20:05:39 +0530

    adds menu component

--- app/src/main/java/com/example/deepflick/MainActivity.java
+++ app/src/main/java/com/example/deepflick/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.deepflick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
     }
 }
