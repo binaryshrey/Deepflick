4c4be06a0e33b07458650c665edf865a9d2d3521
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-04-16 02:23:01 +0530

    adds moviesAdapter

---
+++ app/src/main/java/com/example/deepflick/adapter/MoviesAdapter.java
@@ -0,0 +1,62 @@
+package com.example.deepflick.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.deepflick.R;
+import com.example.deepflick.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
+    public Movie[] mMovieData;
+
+    public MoviesAdapter(Movie[] movie) {
+        mMovieData = movie;
+    }
+    @NonNull
+    @Override
+    public MoviesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        //inflate list item xml into a view
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new MoviesAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapterViewHolder holder, int position) {
+        //set the movie for list item's position
+        String movieToBind = mMovieData[position].getThumbnail();
+        Picasso.get()
+                .load(movieToBind)
+                .into(holder.mThumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mMovieData == null)
+            return 0;
+        return mMovieData.length;
+    }
+
+    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_thumbnail)
+        ImageView mThumbnail;
+
+        public MoviesAdapterViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
