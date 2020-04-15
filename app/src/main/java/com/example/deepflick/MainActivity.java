a059a6ba5218125011e94dd23379ffbfb4bad521
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-04-15 20:11:18 +0530

    adds menu functionality

--- app/src/main/java/com/example/deepflick/MainActivity.java
+++ app/src/main/java/com/example/deepflick/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.deepflick;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemThatWasSelected = item.getItemId();
+        switch (itemThatWasSelected){
+            case R.id.popular:
+                Toast.makeText(this, "Popular Movies", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.top_rated:
+                Toast.makeText(this, "Top Rated Movies", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
