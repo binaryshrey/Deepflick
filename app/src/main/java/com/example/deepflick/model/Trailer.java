b5d0b94033ef69c31bc455ab4a11abd1d599ef0d
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-04-21 19:40:12 +0530

    adds Trailer model

---
+++ app/src/main/java/com/example/deepflick/model/Trailer.java
@@ -0,0 +1,23 @@
+package com.example.deepflick.model;
+
+public class Trailer {
+    //data members
+    private String mId;
+    private String mKey;
+    private String mName;
+
+    //constructor
+    public Trailer(){}
+
+    //getter method
+    public String getId() { return mId; }
+    //setter method
+    public void setId(String mId ){ this.mId = mId; }
+
+    public String getKey() { return mKey; }
+    public void setKey(String mKey ){ this.mKey = mKey; }
+
+    public String getName() { return mName; }
+    public void setName(String mName){ this.mName = mName; }
+
+}
