542c2602de5116559279d2679a427c0a3be49ca6
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-04-21 22:49:29 +0530

    updates TMDBJsonUtils - TrailerJSON

--- app/src/main/java/com/example/deepflick/utils/TMDBJsonUtils.java
+++ app/src/main/java/com/example/deepflick/utils/TMDBJsonUtils.java
@@ -3,23 +3,26 @@ package com.example.deepflick.utils;
 import android.content.Context;
 
 import com.example.deepflick.model.Movie;
+import com.example.deepflick.model.Trailer;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class TMDBJsonUtils {
-    //data members
-    private static final String ROOT_OBJECT = "results";
-    private static final String TITLE_KEY = "title";
-    private static final String THUMBNAIL_URL = "https://image.tmdb.org/t/p/w500";
-    private static final String THUMBNAIL_KEY = "poster_path";
-    private static final String RATING_KEY = "vote_average";
-    private static final String RELEASE_DATE_KEY = "release_date";
-    private static final String ADULT_CONTENT_KEY = "adult";
-    private static final String OVERVIEW_KEY = "overview";
 
     //method to parse json data
     public static Movie[] parseValuesFromJson(Context context, String data) throws JSONException {
+        //data members
+        final String ROOT_OBJECT = "results";
+        final String TITLE_KEY = "title";
+        final String THUMBNAIL_URL = "https://image.tmdb.org/t/p/w500";
+        final String THUMBNAIL_KEY = "poster_path";
+        final String RATING_KEY = "vote_average";
+        final String RELEASE_DATE_KEY = "release_date";
+        final String ADULT_CONTENT_KEY = "adult";
+        final String OVERVIEW_KEY = "overview";
+        final String ID_KEY = "id";
 
         //initialize json object from json string
         JSONObject json = new JSONObject(data);
@@ -40,5 +43,29 @@ public class TMDBJsonUtils {
             results[i] = movie;
         }
         return results;
+    }
+
+    //method to parse Trailer json data
+    public static Trailer[] parseTrailerValuesFromJson(Context context, String data) throws JSONException {
+        //data members
+        final String ROOT_OBJECT = "results";
+        final String TRAILER_KEY_KEY = "key";
+        final String TRAILER_NAME_KEY = "name";
+
+        //initialize json object from json string
+        JSONObject json = new JSONObject(data);
+        //name a json object
+        JSONArray valuesArray = json.getJSONArray(ROOT_OBJECT);
+        Trailer trailerResults[] = new Trailer[valuesArray.length()];
+        for (int i = 0; i < valuesArray.length(); i++) {
+            //creating object of class Movie using new keyword
+            Trailer trailer = new Trailer();
+            //calling setters methods
+            trailer.setKey(valuesArray.getJSONObject(i).optString(TRAILER_KEY_KEY));
+            trailer.setName(valuesArray.getJSONObject(i).optString(TRAILER_NAME_KEY));
+            //storing trailer data
+            trailerResults[i] = trailer;
+        }
+        return trailerResults;
     }
 }
