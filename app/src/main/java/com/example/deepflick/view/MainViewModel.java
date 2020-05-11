88c2d4a3da9c516ce2bb3b89acb8e3929c042c3e
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-05-11 09:27:13 +0530

    adds MainViewModel

---
+++ app/src/main/java/com/example/deepflick/view/MainViewModel.java
@@ -0,0 +1,26 @@
+package com.example.deepflick.view;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.deepflick.database.FavoriteMovie;
+import com.example.deepflick.database.FavoriteMovieDatabase;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private LiveData<List<FavoriteMovie>> movies;
+
+    public MainViewModel(Application application) {
+        super(application);
+        FavoriteMovieDatabase database= FavoriteMovieDatabase.getInstance(this.getApplication());
+        movies = database.movieDao().loadAllFavMovies();
+    }
+
+    public LiveData<List<FavoriteMovie>> getMovies() {
+        return movies;
+    }
+}
